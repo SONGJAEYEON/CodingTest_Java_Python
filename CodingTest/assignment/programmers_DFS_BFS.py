f7f057fa2949aa001332c90d94f43aec56af0643
SONGJAEYEON <shw991027@gmail.com>
2020-08-13 21:29:58 +0900

    2020. 08. 13.
    DFS부터 다시시작

--- CodingTest/assignment/programmers_DFS_BFS.py
+++ CodingTest/assignment/programmers_DFS_BFS.py
@@ -1,1 +1,6 @@
-print("커밋앤푸시를 해볼까!!")
+# 사용할 수 있는 숫자가 담긴 배열 numbers 타켓넘버 target이 매개변수로 주어질때 만드는방법의 수를 return 하도록
+numbers=[1,1,1,1,1]
+numbers_minus=[-1*number for number in numbers ]
+numbers.extend(numbers_minus)
+
+#combination을 만들어서 써야할 것 같은데,,!!
